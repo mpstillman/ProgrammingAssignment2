0aa61cb3ef9574a1bc25839bf51b61ec3458297d
Mike Stillman <mpstillman@yahoo.com>
2014-06-19 20:51:58 -0400

    more comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
+## test changes
+## more comments
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
